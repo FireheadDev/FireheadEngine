c711ee2d8c1c785755583713956aea2e0e713abc
Queejon <queejonjin@gmail.com>
2024-07-22 00:14:14 -0500

    feat: Add a combined image sampler to the descriptor set, and use it to sample the texture when rendering.

--- source/renderer/renderLoop.h
+++ source/renderer/renderLoop.h
@@ -85,10 +85,10 @@ extern "C"
 		bool _frameBufferResized;
 
 		const std::vector<Vertex> _vertices = {  // NOLINT(cppcoreguidelines-avoid-const-or-ref-data-members)
-			{{-0.5f, -0.5f}, {1.f, 0.f, 0.f}},
-			{{0.5f, -0.5f}, {0.f, 1.f, 0.f}},
-			{{0.5f, 0.5f}, {0.f, 0.f, 1.f}},
-			{{-0.5f, 0.5f}, {1.f, 1.f, 1.f}},
+			{{-0.5f, -0.5f}, {1.f, 0.f, 0.f}, {1.f, 0.f}},
+			{{0.5f, -0.5f}, {0.f, 1.f, 0.f}, {0.f, 0.f}},
+			{{0.5f, 0.5f}, {0.f, 0.f, 1.f}, {0.f, 1.f}},
+			{{-0.5f, 0.5f}, {1.f, 1.f, 1.f}, {1.f, 1.f}},
 		};
 		const std::vector<uint16_t> _indices = {  // NOLINT(cppcoreguidelines-avoid-const-or-ref-data-members)
 			0, 1, 2, 2, 3, 0
