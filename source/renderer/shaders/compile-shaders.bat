9a7e2bf3fd7f5c0cc3b9f308a291f10590687a85
Queejon <queejonjin@gmail.com>
2024-07-24 12:12:00 -0500

    feat: move shader directory.

--- source/renderer/shaders/compile-shaders.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-:: TODO: Automate finding of shaders to compile.
-echo Compiling vertex shader
-C:/VulkanSDK/1.3.283.0/Bin/glslc.exe shader.vert -o vert.spv
-echo Compiling fragment shader
-C:/VulkanSDK/1.3.283.0/Bin/glslc.exe shader.frag -o frag.spv
-
-echo Compilations finished!
-pause
